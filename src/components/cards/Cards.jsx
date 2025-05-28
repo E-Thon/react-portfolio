3fd31ed604dfda2e1ebbf4b2e199df30074db639
Emilie Thon <emilie.m.thon@gmail.com>
2025-05-28 14:02:48 +0200

    project card components added

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,58 +1,25 @@
 import "./_Cards.scss";
 
-export default function Cards() {
+export default function Cards({title,
+    url,
+    imageSrc,
+    imageAlt,
+    technologies = []}) {
 
     return (
-        <>
-            <article className="projects__cards">
-                <h3>MyMovies</h3>
-                <a href="https://thon-mymovies.netlify.app/" target="_blank">
-                    <img src="/mymovies-screenshot543x543.png" alt="Screenshot of myMovies webpage" />
-                </a>
+        <article className="projects__cards">
+            <h3>{title}</h3>
+            <a href={url} target="_blank" rel="noopener noreferrer">
+                <img src={imageSrc} alt={imageAlt} />
+            </a>
+            <ul>
+                <li>Technologies used</li>
                 <ul>
-                    <li>Technologies used</li>
-                    <ul>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                        <li>REST API</li>
-                    </ul>
+                    {technologies.map((tech, index) => (
+                        <li key={index}>{tech}</li>
+                    ))}
                 </ul>
-            </article>
-            <article className="projects__cards">
-                <h3>Pokedex</h3>
-                <a href="https://thon-pokedex.netlify.app/" target="_blank">
-                    <img src="/pokedex-screenshot588x588.png" alt="Screenshot of Pokedex webpage" />
-                </a>
-                <ul>
-                    <li>Technologies used</li>
-                    <ul>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                        <li>REST API</li>
-                    </ul>
-                </ul>
-            </article>
-            <article className="projects__cards">
-                <h3>Newsify</h3>
-                <a href="https://thon-newsify.netlify.app/" target="_blank">
-                    <img src="/newsify-screenshot538x538.png" alt="Screenshot of Newslify webpage" />
-                </a>
-                <ul>
-                    <li>Technologies used</li>
-                    <ul>
-                        <li>HTML</li>
-                        <li>SCSS</li>
-                        <li>JavaScript</li>
-                        <li>REST API</li>
-                        <li>Vite</li>
-                        <li>Vitest</li>
-                        <li>Node</li>
-                        <li>webpack</li>
-                    </ul>
-                </ul>
-            </article>
-        </>
+            </ul>
+        </article>
     )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,14 @@
+import { useEffect, useState } from "react";
 import Cards from "../components/cards/Cards";
 import "./_Projects.scss";
 
 export default function Projects() {
+    const [cards, setCards] = useState([])
+    useEffect(() => {
+        fetch('/data.json')
+            .then(response => response.json())
+            .then(data => setCards(data.cards))
+    }, [])
 
     return (
         <>
@@ -10,9 +17,17 @@ export default function Projects() {
                     projects
                 </h1>
                 <div className="projects__cards">
-                    <Cards />
+                    {cards.map((card, index) => (
+                        <Cards
+                            key={index}
+                            title={card.title}
+                            url={card.url}
+                            imageSrc={card.imageSrc}
+                            imageAlt={card.imageAlt}
+                            technologies={card.technologies}
+                        />
+                    ))}
                 </div>
-
             </section>
         </>
     )
