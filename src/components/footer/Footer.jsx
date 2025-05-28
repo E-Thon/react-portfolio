6fb8ce114eb136d46f04f94461d8275806e5356e
Emilie Thon <emilie.m.thon@gmail.com>
2025-05-28 11:33:38 +0200

    footer in progress

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,11 +2,10 @@ import { Link } from "react-router"
 import { FaFacebook, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export default function Footer({ img, adress, number, mail }) {
-
     return (
-        <>
-            <div className="div__contact">
-                <figure className="div__portrait">
+        <footer>
+            <div className="footer__contact">
+                <figure className="footer__portrait">
                     <img src={img} className="portrait" alt="Portrait of Emilie Malene Thon" />
                 </figure>
                 <address>
@@ -15,20 +14,20 @@ export default function Footer({ img, adress, number, mail }) {
                     <p>Mail: {mail}</p>
                 </address>
             </div>
-            <ul className="div__somes">
-                <li className="div__some">
-                    <Link href="https://www.facebook.com/emi.m.thon"><FaFacebook/>
-                    </Link></li>
-                <li className="div__some">
-                    <Link href="https://www.instagram.com/emi.thon/"><FaInstagram/></Link>
-                    </li>
-                <li className="div__some">
-                    <Link href="https://www.linkedin.com/in/emilie-thon-257943325/"><FaLinkedin/></Link>
-                    </li>
-                <li className="div__some">
-                    <Link href="https://github.com/E-Thon"><FaGithub/></Link>
-                    </li>
+            <ul className="footer__somes">
+                <li className="footer__some">
+                    <Link href="https://www.facebook.com/emi.m.thon"><FaFacebook /></Link>
+                </li>
+                <li className="footer__some">
+                    <Link href="https://www.instagram.com/emi.thon/"><FaInstagram /></Link>
+                </li>
+                <li className="footer__some">
+                    <Link href="https://www.linkedin.com/in/emilie-thon-257943325/"><FaLinkedin /></Link>
+                </li>
+                <li className="footer__some">
+                    <Link href="https://github.com/E-Thon"><FaGithub /></Link>
+                </li>
             </ul>
-        </>
+        </footer>
     )
 }
