5e648609d8e4b4dd7447cf063622fc1286051ab0
Emilie Thon <emilie.m.thon@gmail.com>
2025-05-28 08:57:50 +0200

    contact handling added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,33 @@
-export default function Footer({copyright}) {
+import { Link } from "react-router"
 
-    return(
-        <p>
-            {copyright}
-        </p>
+export default function Footer({ img, adress, number, mail }) {
+
+    return (
+        <>
+            <div className="div__contact">
+                <figure className="div__portrait">
+                    <img src={img} className="portrait" alt="Portrait of Emilie Malene Thon" />
+                </figure>
+                <address>
+                    <p>{adress}</p>
+                    <p>Mobile: {number}</p>
+                    <p>Mail: {mail}</p>
+                </address>
+            </div>
+            <ul className="div__somes">
+                <li className="div__some">
+                    <Link href="https://www.facebook.com/emi.m.thon"><i className="fa-brands fa-facebook-f"></i>
+                    </Link></li>
+                <li className="div__some">
+                    <Link href="https://www.instagram.com/emi.thon/"><i className="fa-brands fa-instagram"></i></Link>
+                    </li>
+                <li className="div__some">
+                    <Link href="https://www.linkedin.com/in/emilie-thon-257943325/"><i className="fa-brands fa-linkedin-in"></i></Link>
+                    </li>
+                <li className="div__some">
+                    <Link href="https://github.com/E-Thon"><i className="fa-brands fa-github"></i></Link>
+                    </li>
+            </ul>
+        </>
     )
 }
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,15 @@
+
+export default function Projects() {
+
+    return (
+        <>
+            <h1>
+                About
+            </h1>
+            <div class="about">
+                <p>About N/A</p>
+                <span >... under construction</span>
+            </div>
+        </>
+    )
+}
