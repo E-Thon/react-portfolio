cec8616238a1c405e8bae54423d522904989883a
Emilie Thon <emilie.m.thon@gmail.com>
2025-05-28 09:00:18 +0200

    actions corrected

--- src/utilities/actions.js
+++ src/utilities/actions.js
@@ -17,19 +17,6 @@ export async function handleSubmit({ request }) {
         const errors = z.treeifyError(result.error)
         return errors.properties
     }
-    
-    // her: fetch... method post:
-    const response = await fetch('https://jsonplaceholder.typicode.com/users', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(result.data)
-    })
-
-    if (!response.ok) {
-        throw new Error("Could not save data")
-    }
 
     redirect("/") // evt til takkeside/ kvitteringsside (nu index)
 }
