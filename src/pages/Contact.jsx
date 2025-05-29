b7f9018aebc56606aa7d48ac7d8d0e5627e31382
Emilie Thon <emilie.m.thon@gmail.com>
2025-05-29 14:43:16 +0200

    contact added and emailJS corrected

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,29 +1,27 @@
-import { Form, useActionData } from "react-router"
+import Formula from "../components/formula/Formula";
 import "./_Contact.scss";
 
 export default function Contact() {
-    const errors = useActionData();
     return (
         <>
             <h1>Contact</h1>
-            <Form method="post">
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" placeholder="Name" />
-                    <p>{errors && errors?.name?.errors[0]}</p>
-                </div>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" name="email" placeholder="Email" />
-                    <p>{errors && errors?.email?.errors[0]}</p>
-                </div>
-                <div>
-                    <label htmlFor="message">Message:</label>
-                    <textarea name="message" id="message" placeholder="Message"></textarea>
-                    <p>{errors && errors?.message?.errors[0]}</p>
-                </div>
-                <button type="submit">Submit</button>
-            </Form>
+            <section className="contact">
+                <h2>
+                    Hello, thank you for your interest!
+                </h2>
+                <p>
+                    I am currently placed in Nørrebro, Copenhagen where I live.
+                </p>
+                <p>
+                    Get in contact by phone on <a href="tel:+4560615218"
+                        className="section__content__text__link">+45 60 61 52 18</a>. If I am unavailable, please leave a message and I will get back to you as soon as possible.
+                </p>
+                <p>
+                    If you are more of a texter, please, send an e-mail to <a href="mailto:emilie.m.thon@gmail.com?subject=Mail%20from%20portfolio&body=Direct%20contact%20"
+                        target="_blank">emilie.m.thon@gmail.com</a>, or fill out the formula below.
+                </p>
+            </section>
+            <Formula />
         </>
     )
 }
