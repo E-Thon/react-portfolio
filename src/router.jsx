8ab011195c983406ef13f45b897627c08d5220f9
Emilie Thon <emilie.m.thon@gmail.com>
2025-06-02 14:06:03 +0200

    end of day

--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,13 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 import Loading from "./components/loading/Loading";
+import { ErrorBoundary } from "./components/error/ErrorBoundary";
 import Layout from "./Layout";
+import { handleSubmit } from "./utilities/actions";
 import { getProjects } from "./utilities/typicode";
 
 const router = createBrowserRouter([
@@ -9,11 +15,30 @@ const router = createBrowserRouter([
         path: "/",
         element: <Layout />,
         hydrateFallbackElement: <Loading />,
+        errorElement: <ErrorBoundary />,
         children: [
             {
                 index: true,
                 element: <Home />,
                 loader: getProjects,
+            },
+            {
+                path: "about",
+                element: <About />,
+            },
+            {
+                path: "projects",
+                element: <Projects />,
+                loader: getProjects,
+            },
+            {
+                path: "contact",
+                element: <Contact />,
+                action: handleSubmit,
+            },
+            {
+                path: "*",
+                element: <NotFound />
             }
         ]
     }
